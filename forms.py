ea777f62924b43b6e39b93684c26f45254c87616
Kateleen Issa <kateleenissa@Jimis-MacBook-Air-2.local>
2026-04-15 15:32:47 -0400

    Add password length and complexity validation to registration form.

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField, PasswordField, EmailField
 from wtforms.fields.numeric import IntegerField
-from wtforms.validators import DataRequired, Length, Email, NumberRange, URL, Optional, EqualTo
+from wtforms.validators import DataRequired, Length, Email, NumberRange, URL, Optional, EqualTo, Regexp
 from email_validator import validate_email, EmailNotValidError
 
 
@@ -19,42 +19,66 @@ def normalize_email(email):
 
 
 class CommentForm(FlaskForm):
-    comment = TextAreaField("Write a comment below", validators=[DataRequired(), Length(max=2000, message="Comment must be 2000 characters or fewer.")],
-                             render_kw={"placeholder": "Enter your thoughts", "class": "form-control"})
+    comment = TextAreaField("Write a comment below", validators=[DataRequired(), Length(max=2000,
+                                                                                        message="Comment must be 2000 characters or fewer.")],
+                            render_kw={"placeholder": "Enter your thoughts", "class": "form-control"})
     submit = SubmitField('Post', render_kw={"class": "mb-3"})
 
+
 class RegisterForm(FlaskForm):
-    first_name = StringField('First Name*', validators=[DataRequired()], render_kw={"placeholder": "First name", "class": "form-control"})
-    last_name = StringField('Last Name*', validators=[DataRequired()], render_kw={"placeholder": "Last name", "class": "form-control"})
-    email = EmailField('Email*', validators=[Email(check_deliverability=True), DataRequired()], filters=[normalize_email], render_kw={"placeholder": "Email", "class": "form-control"})
-    password = PasswordField('Password*', validators=[DataRequired()], render_kw={"placeholder": "Password", "class": "form-control mb-4"})
-    confirm_password = PasswordField('Confirm Password*', validators=[EqualTo("password", message="Passwords do not match."),DataRequired()], render_kw={"placeholder": "Re-enter password", "class": "form-control mb-4"})
+    first_name = StringField('First Name*', validators=[DataRequired()],
+                             render_kw={"placeholder": "First name", "class": "form-control"})
+    last_name = StringField('Last Name*', validators=[DataRequired()],
+                            render_kw={"placeholder": "Last name", "class": "form-control"})
+    email = EmailField('Email*', validators=[Email(check_deliverability=True), DataRequired()],
+                       filters=[normalize_email], render_kw={"placeholder": "Email", "class": "form-control"})
+    password = PasswordField('Password*', validators=[DataRequired(), Length(min=8,
+                                                                             message="Password must be at least 8 characters long."),
+                                                      Regexp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%]).+$',
+                                                             message="Password must include at least one lowercase letter, one uppercase letter, one number, and one special character.")],
+                             render_kw={"placeholder": "Password", "class": "form-control mb-4"})
+    confirm_password = PasswordField('Confirm Password*',
+                                     validators=[EqualTo("password", message="Passwords do not match."),
+                                                 DataRequired()],
+                                     render_kw={"placeholder": "Re-enter password", "class": "form-control mb-4"})
     submit = SubmitField('Sign Up', render_kw={"class": "mb-4"})
 
 
 class LoginForm(FlaskForm):
-    email = EmailField('Email*', validators=[Email(), DataRequired()], render_kw={"placeholder": "Email", "class": "form-control"})
-    password = PasswordField('Password*', validators=[DataRequired()], render_kw={"placeholder": "Password", "class": "form-control mb-4"})
+    email = EmailField('Email*', validators=[Email(), DataRequired()],
+                       render_kw={"placeholder": "Email", "class": "form-control"})
+    password = PasswordField('Password*', validators=[DataRequired()],
+                             render_kw={"placeholder": "Password", "class": "form-control mb-4"})
     submit = SubmitField('Login', render_kw={"class": "mb-4"})
 
 
 class ContactForm(FlaskForm):
     name = EmailField('Full Name*', validators=[DataRequired()],
-                       render_kw={"placeholder": "Full Name", "class": "form-control"})
+                      render_kw={"placeholder": "Full Name", "class": "form-control"})
     email = EmailField('Email*', validators=[Email(), DataRequired()],
                        render_kw={"placeholder": "Email", "class": "form-control"})
     message = TextAreaField('Message*', validators=[DataRequired()],
-                             render_kw={"placeholder": "Enter your message", "class": "form-control mb-4"})
+                            render_kw={"placeholder": "Enter your message", "class": "form-control mb-4"})
     submit = SubmitField('Send', render_kw={"class": "mb-4"})
 
 
 class CreatePostForm(FlaskForm):
-    title = StringField('Title*', validators=[DataRequired()], render_kw={"placeholder": "Title", "class": "form-control"})
-    subtitle = StringField('Subtitle*', validators=[DataRequired()], render_kw={"placeholder": "Subtitle", "class": "form-control"})
-    location = StringField('Destination ✈️ (e.g. City, Prov/State, Country)*', validators=[DataRequired()], render_kw={"placeholder": "Where", "class": "form-control"})
-    num_times_visited = IntegerField('How many times have you visited?*', validators=[NumberRange(min=1, message="Value must be above 0."), DataRequired()], render_kw={"placeholder": "How often", "class": "form-control"})
-    body = TextAreaField('Body*', validators=[DataRequired(), Length(max=10000, message="Body must be 10,000 characters or fewer.")], render_kw={"placeholder": "Tell us about it", "class": "form-control"})
+    title = StringField('Title*', validators=[DataRequired()],
+                        render_kw={"placeholder": "Title", "class": "form-control"})
+    subtitle = StringField('Subtitle*', validators=[DataRequired()],
+                           render_kw={"placeholder": "Subtitle", "class": "form-control"})
+    location = StringField('Destination ✈️ (e.g. City, Prov/State, Country)*', validators=[DataRequired()],
+                           render_kw={"placeholder": "Where", "class": "form-control"})
+    num_times_visited = IntegerField('How many times have you visited?*',
+                                     validators=[NumberRange(min=1, message="Value must be above 0."), DataRequired()],
+                                     render_kw={"placeholder": "How often", "class": "form-control"})
+    body = TextAreaField('Body*', validators=[DataRequired(),
+                                              Length(max=10000, message="Body must be 10,000 characters or fewer.")],
+                         render_kw={"placeholder": "Tell us about it", "class": "form-control"})
     visit_again = BooleanField('Would you visit again?')
-    rating = SelectField('How many stars?', choices=["⭐", "⭐⭐", "⭐⭐⭐", "⭐⭐⭐⭐", "⭐⭐⭐⭐⭐"], validators=[DataRequired()], render_kw={"class": "form-control"})
-    img_url = StringField('Image URL', validators=[Optional(), URL(), Length(max=2000, message="Image URL must be 2000 characters or fewer.")], render_kw={"placeholder": "Image Link", "class": "form-control mb-4"})
+    rating = SelectField('How many stars?', choices=["⭐", "⭐⭐", "⭐⭐⭐", "⭐⭐⭐⭐", "⭐⭐⭐⭐⭐"], validators=[DataRequired()],
+                         render_kw={"class": "form-control"})
+    img_url = StringField('Image URL', validators=[Optional(), URL(), Length(max=2000,
+                                                                             message="Image URL must be 2000 characters or fewer.")],
+                          render_kw={"placeholder": "Image Link", "class": "form-control mb-4"})
     submit = SubmitField('Submit', render_kw={"class": "mb-3"})
